facf2b2b46d3b5560cc98bc3d86b134f27ec1fc5
shalure10lure <yessicarado@gmail.com>
2025-11-20 23:46:27 -0400

    feat:addign model user

--- TareaTW/Models/Book.cs
+++ TareaTW/Models/Book.cs
@@ -1,6 +1,11 @@
 ﻿namespace TareaTW.Models
 {
-    public class Student
+    public class Book
     {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; } 
+        public int Year { get; set; }
+        public string? Description { get; set; }
     }
 }
--- TareaTW/Models/User.cs
+++ TareaTW/Models/User.cs
@@ -1,6 +1,16 @@
-﻿namespace TareaTW.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace TareaTW.Models
 {
     public class User
     {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; } = "User";//"User" | "Admin"
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiresAt { get; set; }
+        public DateTime? RefreshTokenRevokedAt { get; set; }
+        public string? CurrentJwtId { get; set; }
     }
 }
