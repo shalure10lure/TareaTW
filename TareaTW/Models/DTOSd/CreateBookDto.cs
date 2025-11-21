60684dbd8720fc40ce438bef3715151ffba42188
shalure10lure <yessicarado@gmail.com>
2025-11-20 23:56:30 -0400

    feat:addign dtos book

---
+++ TareaTW/Models/DTOSd/CreateBookDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TareaTW.Models.DTOSd
+{
+    public class CreateBookDto
+    {
+        [Required]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ TareaTW/Models/DTOSd/UpdateBookDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TareaTW.Models.DTOSd
+{
+    public class UpdateBookDto
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string? Description { get; set; }
+    }
+}
